80f12e6ca920b5fad10d9a6f332ef97c09ee26d9
monosus_yamamoto <y.yamamoto@monosus.co.jp>
2023-09-24 16:20:18 +0900

    feat: microCMS連携

---
+++ src/pages/mami/[id].tsx
@@ -0,0 +1,30 @@
+import { client } from "../../../libs/client";
+import styles from "@/styles/Home.module.scss";
+
+export const getStaticProps = async (context: any) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "mami", contentId: id });
+
+  return {
+    props: {
+      mami: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "mami" });
+
+  const paths = data.contents.map((content: any) => `/mami/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export default function MamiId({ mami }: any) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{mami.title}</h1>
+      <p className={styles.publishedAt}>{mami.publishedAt}</p>
+      <div className={styles.post} dangerouslySetInnerHTML={{ __html: `${mami.body}` }}></div>
+    </main>
+  );
+}
